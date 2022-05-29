42f6ced699754d977dc6aed9951cd96b61673439
mila0523 <102968933+u21589276@users.noreply.github.com>
2022-05-29 23:21:16 +0200

    added two new views and action results for the file controller

---
+++ u21589276_HW03/Controllers/FileController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace u21589276_HW03.Controllers
+{
+    public class FileController : Controller
+    {
+        // GET: File
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DeleteFile()
+        {
+            return View();
+        }
+    }
+}
--- u21589276_HW03/Models/FileModel.cs
+++ u21589276_HW03/Models/FileModel.cs
@@ -10,5 +10,11 @@ namespace u21589276_HW03.Models
     {
         [Display(Name = "File Name")]
         public string FileName { get; set; }
+
+        [Display(Name = "Download option")]
+        public string DownloadOption { get; set; }
+
+        [Display(Name = "Delete Option")]
+        public string DeleteOption { get; set; }
     }
 }
