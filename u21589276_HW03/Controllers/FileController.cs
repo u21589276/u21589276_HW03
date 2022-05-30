e168c931e047cf83db690b055e145ada4cc6994b
mila0523 <102968933+u21589276@users.noreply.github.com>
2022-05-30 19:06:40 +0200

    upload feature update

--- u21589276_HW03/Controllers/FileController.cs
+++ u21589276_HW03/Controllers/FileController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
 
 namespace u21589276_HW03.Controllers
 {
--- u21589276_HW03/Controllers/HomeController.cs
+++ u21589276_HW03/Controllers/HomeController.cs
@@ -3,14 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
 
 namespace u21589276_HW03.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+
+
+        //File Upload Recieve
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase fileupload)//INSIDE HOME
+        {
+            
+            if (fileupload != null && fileupload.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(fileupload.FileName);
+
+                var path = Path.Combine(Server.MapPath("~/Media/Documents"), fileName);
+
+                fileupload.SaveAs(path);
+            }
+            return RedirectToAction("Index");
         }
 
         public ActionResult About()
@@ -19,6 +38,8 @@ namespace u21589276_HW03.Controllers
 
             return View();
         }
+
+        //============================================================================
         public ActionResult Files()
         {
             ViewBag.Message = "Your Files.";
